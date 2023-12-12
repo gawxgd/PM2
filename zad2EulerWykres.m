a8398a1f97e8b0909aba63b0ef1f1ee0a7b669e7
gawxgd <128865159+gawxgd@users.noreply.github.com>
2023-12-12 23:00:23 +0100

    raport do zad 2 włącznie

--- zad2EulerWykres.m
+++ zad2EulerWykres.m
@@ -29,15 +29,20 @@ for i=1:length(combs)
     sols(i) = Jx(zad2implicitEulerY(combs(i,1),tDane,yDane,combs(i,2:end)));
     sols2(i) = Jx2(zad2implicitEulerY(combs2(i,1),tDane,xDane,combs2(i,2:end)));
 end
+
+options = optimset('fminsearch');
+options.MaxIter = 4000;
+options.MaxFunEvals = 5000;
+
 minimum = min(sols);
 Rminimum = combs(sols == minimum,:);
 tempFunc = @(x) Jx(zad2implicitEulerY(x(1),tDane,yDane,x(2:end)));
-opt_PopX = fminsearch(tempFunc,Rminimum)
+[opt_PopX,fval] = fminsearch(tempFunc,Rminimum,options)
 
 minimum2 = min(sols2);
 Rminimum2 = combs2(sols2 == minimum2,:);
 tempFunc2 = @(x) Jx2(zad2implicitEulerY(x(1),tDane,xDane,x(2:end)));
-opt_PopY = fminsearch(tempFunc2,Rminimum2)
+[opt_PopY,fval] = fminsearch(tempFunc2,Rminimum2,options)
 
 figure(1)
 plot(tDane,zad2implicitEulerY(opt_PopX(1),tDane,yDane,opt_PopX(1,2:end)))
@@ -45,6 +50,8 @@ hold on
 plot(tDane,xDane)
 title("Wykres populacji x niejawny Euler")
 legend("populacja przybliżona","populacja dokładna")
+xlabel("t - czas")
+ylabel("liczność populacji")
 
 figure(2)
 plot(tDane,zad2implicitEulerY(opt_PopY(1),tDane,xDane,opt_PopY(1,2:end)))
@@ -52,3 +59,5 @@ hold on
 plot(tDane,yDane)
 title("Wykres populacji y niejawny Euler")
 legend("populacja przybliżona","populacja dokładna")
+xlabel("t - czas")
+ylabel("liczność populacji")
